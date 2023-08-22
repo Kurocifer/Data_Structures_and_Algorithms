757e0fc6c228fc1b877f9b9a5d14f46625ae2659
Cifer-ti <draculemihawk232@gmail.com>
2023-08-22 18:56:46 +0100

    ...

--- (2)_Algorithm_design/exercises/4/fibcomp.c
+++ (2)_Algorithm_design/exercises/4/fibcomp.c
@@ -4,6 +4,7 @@
 #define MAX_TEST 10
 
 int fibcomp(int n);
+int findFibonacciPosition(int fibonacciNumber);
 
 int main(void)
 {
@@ -16,29 +17,70 @@ int main(void)
     
 }
 
+int findFibonacciPosition(int fibonacciNumber) {
+    int current = 0;
+    int next = 1;
+    int position = 1;
+
+    while (current < fibonacciNumber) {
+        int temp = current;
+        current = next;
+        next = temp + current;
+        position++;
+    }
+
+    if (current == fibonacciNumber) {
+        return position;
+    } else {
+        return -1;  // Fibonacci number not found
+    }
+}
+
+int fibcomp(int n)
+{
+    int fibn = 0, fib1 = 1, fib2 = 0;
+    int pos = 1;
+
+    while(fibn < n) {
+        fib2 = fib1;
+        fib1 = fibn;
+        fibn = fib1 + fib2;
+        pos++;
+    }
+
+    if(fibn == n)
+        return pos;
+    return -1;
+}
+
+/*
 int fibcomp(int n)
 {
     int fibn, fib1, fib2;
     int pos;
 
-    if(n < 0)
+    if(n <= 0)
         return -1;
     
-    if(n == 0)
+    if(n == 0 )
         return 1;
 
-    if(n == 1)
+    if((n == 1))
         return 2;
     
-    pos = 3;
     fibn = 0;
     fib1 = 1;
     fib2 = 0;
 
-    while(fibn < n) {
+    for(pos = 3; fibn < n;) {
         fibn = fib1 + fib2;
         fib1 = fibn;
         fib2 = fib1;
         pos++;
     }
+
+    if(fibn == n)
+        return pos;
+    return -2;
 }
+*/
