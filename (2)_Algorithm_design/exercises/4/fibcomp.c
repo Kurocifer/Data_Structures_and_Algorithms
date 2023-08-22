2135334c8d4518a55dcf84cb972d1e388cf8d2f2
Cifer-ti <draculemihawk232@gmail.com>
2023-08-22 19:05:55 +0100

    ...

--- (2)_Algorithm_design/exercises/4/fibcomp.c
+++ (2)_Algorithm_design/exercises/4/fibcomp.c
@@ -17,24 +17,6 @@ int main(void)
     
 }
 
-int findFibonacciPosition(int fibonacciNumber) {
-    int current = 0;
-    int next = 1;
-    int position = 1;
-
-    while (current < fibonacciNumber) {
-        int temp = current;
-        current = next;
-        next = temp + current;
-        position++;
-    }
-
-    if (current == fibonacciNumber) {
-        return position;
-    } else {
-        return -1;  // Fibonacci number not found
-    }
-}
 
 int fibcomp(int n)
 {
@@ -48,39 +30,9 @@ int fibcomp(int n)
         pos++;
     }
 
-    if(fibn == n)
+    if(fibn == n) {
         return pos;
+    }
     return -1;
 }
 
-/*
-int fibcomp(int n)
-{
-    int fibn, fib1, fib2;
-    int pos;
-
-    if(n <= 0)
-        return -1;
-    
-    if(n == 0 )
-        return 1;
-
-    if((n == 1))
-        return 2;
-    
-    fibn = 0;
-    fib1 = 1;
-    fib2 = 0;
-
-    for(pos = 3; fibn < n;) {
-        fibn = fib1 + fib2;
-        fib1 = fibn;
-        fib2 = fib1;
-        pos++;
-    }
-
-    if(fibn == n)
-        return pos;
-    return -2;
-}
-*/
