f6a9972729ab6f248f093e3c6ecb16269f0cf138
Cifer-ti <draculemihawk232@gmail.com>
2023-08-22 20:08:20 +0100

    commented funcition

--- (2)_Algorithm_design/exercises/4/fibcomp.c
+++ (2)_Algorithm_design/exercises/4/fibcomp.c
@@ -3,22 +3,24 @@
 
 #define MAX_TEST 10
 
-int fibcomp(int n);
-int findFibonacciPosition(int fibonacciNumber);
+int fibposition(int n);
 
 int main(void)
 {
     int i;
 
 	for(i = -1; i <= MAX_TEST; i++)
-		printf("\ti: %2d\tfib(%2d): %d\n", i, i, fibcomp(i));
+		printf("\ti: %2d\tfib(%2d): %d\n", i, i, fibpositon(i));
 
 	return 0;
     
 }
 
-
-int fibcomp(int n)
+/*fibpositon: Takes a fibonancci number, and returns
+               it's position in the series.
+n- number to search for position
+Return- position if number is of the series, -1 if not*/
+int fibpositon(int n)
 {
     int fibn = 0, fib1 = 1, fib2 = 0;
     int pos = 1;
