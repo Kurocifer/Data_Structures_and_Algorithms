a91b02e85f62c74706d32420d5868ee84f75685d
Cifer-ti <draculemihawk232@gmail.com>
2023-09-17 13:43:42 +0100

    sequential search

---
+++ 8_Searching_Algo/seqential_search.c
@@ -0,0 +1,22 @@
+/* sequential search */
+
+/**
+ * seq_search: search a table of records for a particular key.
+ * 
+ * data[]- table(array) to be searched
+ * size- size of the table(array)
+ * key- item searched for.
+ * 
+ * Return- Returns the array index of the key else if not found returns -1.
+*/
+int seq_search(int data[], int size, int key)
+{
+    int i;
+
+    i = size - 1;
+
+    for(i = size -1; i >= 0 && data[i] != key; i--)
+        ;
+    
+    return i;
+}
