508b117d3e1ca4bd4c339e0e07f693a993ee3e94
Cifer-ti <draculemihawk232@gmail.com>
2023-09-18 21:41:18 +0100

    indexed sequential search

---
+++ 8_Searching_Algo/sequential_search/index_seq_search.c
@@ -0,0 +1,33 @@
+#include "seq_search.h"
+
+struct index {
+    int val;    /* The key value each element represent */
+    int slot;   /* The index that points into the main table */
+};
+
+int index_search(int key, Index idx[], int idx_size, 
+                    int data[], int data_size) 
+{
+    int i, size, key_index;
+
+    if(key < data[0])       /* Initial test for non existing key */
+        return NOT_FOUND;
+
+    /* Scan index for key */
+    for(i = 0; i < idx_size && key >= idx[i + 1].val; i++)
+        ;
+
+    /* determine segment size */
+    if(i == idx_size)
+        size = size - idx[i].slot;
+    else
+        size = idx[i + 1].slot - idx[i].slot;
+    
+    /* scan data table */
+    key_index = seq_search2(data[idx[i].slot], size; key);
+
+    if(key_index >= 0)
+        key_index += idx[i].slot;
+    
+    return key_index;
+}
--- 8_Searching_Algo/sequential_search/seq_search.h
+++ 8_Searching_Algo/sequential_search/seq_search.h
@@ -1,11 +1,14 @@
 #ifndef SEQ_SEARCH_H
 #define SEQ_SEARCH_H
 
+#define NOT_FOUND -2
+
+typedef struct index Index;
 /**
  * seq_search: search a table of records for a particular key.
  * 
- * data[]- table(array) to be searched
- * size- size of the table(array)
+ * data[]- array to be searched.
+ * size- size of the array to be searched.
  * key- item searched for.
  * 
  * Return- Returns the array index of the key else if not found returns -1.
@@ -16,11 +19,26 @@ int seq_search(int data[], int size, int key);
  * seq_search2: An ordered linear search through the array.
  * 
  * data[]- table(array) to be searched
- * size- size of the table(array)
+ * size- size of the array to be searched
  * key- item searched for.
  * 
- * Return- Returns the array index of the key else if not found returns -1.
+ * Return- If it finds the key, it returns it's index else returns
+ *         the value NOT_FOUND which is less than 0.
 */
 int seq_search2(int data[], int size, int key);
+
+/**
+ * index_search: An indexed search through the array.
+ * 
+ * key- item searched for.
+ * idx[]- The index array.
+ * data[]- The array to be searched.
+ * data_size- The size of the arry to be searched.
+ * 
+ * Return- If it findS the key it returns it's index in the data array
+ *         else if not returns the value NOT_FOUND which is less than 0.
+*/
+int index_search(int key, Index idx[], int idx_size, 
+                    int data[], int data_size);
 
 #endif
